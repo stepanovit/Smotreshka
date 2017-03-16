576dd792ddf9d830ab11809a10ab4ceb70dcfbe8
Andrey <bigzero@yandex.ru>
2017-03-17 01:28:17 +0300

    start

---
+++ Http.php
@@ -0,0 +1,79 @@
+<?php
+
+class Http{
+    public $curl;
+    public $url;
+    public $method;
+    public $request;
+    public $response;
+    public $info;
+
+    public function __construct($url, $method = 'GET', $data = '', $options = array()){
+        $this->url = $url;
+        $this->method = $method;
+        $this->request = $data;
+    }
+
+    public static function get($url){
+        $http = new Http($url);
+        $http->process();
+        return $http;
+    }
+
+    public static function post($url, $data){
+        $http = new Http($url, 'POST', $data);
+        $http->process();
+        return $http;
+    }
+
+    public static function delete($url, $data){
+        $http = new Http($url, 'DELETE', $data);
+        $http->process();
+        return $http;
+    }
+
+    public static function put($url, $data){
+        $http = new Http($url, 'PUT', $data);
+        $http->process();
+        return $http;
+    }
+
+    public static function patch($url, $data){
+        $http = new Http($url, 'PATCH', $data);
+        $http->process();
+        return $http;
+    }
+
+    public function process(){
+        $this->curl = curl_init($this->url);
+        curl_setopt($this->curl, CURLOPT_URL, $this->url);
+        $this->set_options();
+        $this->exec();
+    }
+
+    public function json(){
+        return json_decode($this->response);
+    }
+
+    public function xml(){
+        $xml = new SimpleXMLElement($this->response);
+        return json_decode(json_encode($xml), FALSE);
+    }
+
+    private function set_options(){
+        if($this->method !== 'GET'){
+            curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, $this->method);
+        }
+        if($this->method == "PUT" || $this->method == "PATCH" || ($this->method == "POST") || ($this->method == "DELETE")) {
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, $this->request);
+        }
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+    }
+
+    private function exec(){
+        $result = curl_exec($this->curl);
+        $this->info = curl_getinfo ($this->curl);
+        curl_close($this->curl);
+        $this->response = $result;
+    }
+}
---
+++ Smotreshka.php
@@ -0,0 +1,89 @@
+<?php
+require_once('Http.php');
+
+class Smotreshka{
+    public $operator;
+    public $node;
+    private $url;
+
+    public function __construct($operator, $node){
+        $this->operator = $operator;
+        $this->node = $node;
+        $this->url = 'http://'.$this->operator.'.'.$this->node.'.lfstrm.tv/v2/';
+    }
+
+    public function account_create($email, $login = null, $password = null ,$purchases = null, $info = null){
+        $data = array(
+            'email' => $email
+        );
+        if (isset($login)) $data['login'] = $login;
+        if (isset($password)) $data['password'] = $password;
+        if (isset($purchases)) $data['purchases'] = $purchases;
+        if (isset($info)) $data['info'] = $info;
+
+        $http = Http::post($this->url.'accounts', json_encode($data));
+        return $http->json();
+    }
+
+    public function account_show($id){
+        $http = Http::get($this->url.'accounts/'.$id);
+        return $http->json();
+    }
+
+    public function account_update($id, $info){
+        $http = Http::post($this->url.'accounts/'.$id, json_encode(array('info' => $info)));
+        return $http->json();
+    }
+
+    public function account_reset_password($id, $password){
+        if (isset($password)) {$data = array('password' => $password);} else {$data = array();}
+        $http = Http::post($this->url.'accounts/'.$id.'/reset-password', json_decode($data));
+        return $http->json();
+    }
+
+    public function account_delete($id){
+        $http = Http::delete($this->url.'accounts/'.$id, json_encode(array()));
+        return $http->json();
+    }
+
+    public function accounts($count = null, $page = null){
+        $params = array();
+        if (isset($page)) $params['page_num'] = $page;
+        if (isset($count)) $data['page_sie'] = $count;
+
+        if (count($params) > 0) $query = '?'.http_build_query($params);
+
+        $http = Http::get($this->url.'accounts'.$query);
+        return $http->json();
+    }
+
+    public function account_subscriptions($id){
+        $http = Http::get($this->url.'accounts/'.$id.'/subscriptions');
+        return $http->json();
+    }
+
+    public function account_subscriptions_create($id, $subscription_id){
+        $data = array(
+            'id' => $subscription_id,
+            'valid' => true
+        );
+
+        $http = Http::post($this->url.'accounts/'.$id.'/subscriptions', json_encode($data));
+        return $http->json();
+    }
+
+    public function account_subscriptions_delete($id, $subscription_id){
+        $data = array(
+            'id' => $subscription_id,
+            'valid' => false
+        );
+
+        $http = Http::post($this->url.'accounts/'.$id.'/subscriptions', json_encode($data));
+        return $http->json();
+    }
+
+    public function get_subscriptions(){
+        $http = Http::get($this->url.'subscriptions');
+        return $http->json();
+    }
+}
